d900e843c37ea554dfd3dd59a9802dea32d51d88
Himani <himanimanjunath05@gmail.com>
2025-04-20 00:53:15 -0700

    Update popup.js
    lines 1-5 by nancy and one edit on line 14 (the fontSize: selectedFontSize)

--- focusMode/popup.js
+++ focusMode/popup.js
@@ -1,8 +1,16 @@
+let selectedFontSize = "default";
+
+document.getElementById("fontSize").addEventListener("change", (e) => {
+  selectedFontSize = e.target.value;
+ });
+ 
+
 // listen for clicks on toggle button
 document.getElementById("toggle").addEventListener("click", async () => {
   // get the current active tab
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   // send message to the content script to toggle focus mode
-  chrome.tabs.sendMessage(tab.id, { action: "toggleFocusMode" });
+  chrome.tabs.sendMessage(tab.id, { action: "toggleFocusMode", fontSize: selectedFontSize
+  });
 });
